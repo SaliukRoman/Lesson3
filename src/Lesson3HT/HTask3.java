90adb4558a7267711deb6f6f326ba3fe632fde0f
Saliuk Roman <saliuk.roman@gmail.com>
2022-09-10 16:22:41 +0300

    нн

--- src/Lesson3HT/HTask3.java
+++ src/Lesson3HT/HTask3.java
@@ -3,25 +3,25 @@ package Lesson3HT;
 
 public class HTask3 {
     public static void main(String[] args) {
-        int[] arrRandom = {11, 22, 32, 5, 87, 99, };
-        int array = arrRandom[0];
-        for (int i : arrRandom) {
-            if (i < array) {
-                array = i;
-                System.out.println("Найменший елемент масива " + array);
-            }
-        }
-        for (int i : arrRandom) {
-            if (i > array) {
-                array = i;
-                System.out.println("Найбільший елемент масива " + array);
+        int[] arrRandom = {11, 22, 32, 5, 87, 99};
+        int min = arrRandom[0];
+        int max = arrRandom[0];
+        for (int i = 0; i < arrRandom.length; i++) {
+            if (arrRandom[i] > max) {
+                max = arrRandom[i];
 
             }
+        }
+        for (int n = 0; n < arrRandom.length; n++) {
+            if (arrRandom[n] < min) {
+                min = arrRandom[n];
+
+
+            }
 
         }
-
+        System.out.println(min);
+        System.out.println(max);
     }
 
 }
-
-
